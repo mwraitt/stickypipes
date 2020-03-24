134bfe688b72e5f21feacee1849bce5d05c74f6e
mike <mwraitt@gmail.com>
2020-03-24 11:29:58 -0400

    streaming values to client
    updated readme with test steps

--- stickypipes.api/Services/CacheService.cs
+++ stickypipes.api/Services/CacheService.cs
@@ -1,34 +1,75 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Concurrent;
+using System.Reactive.Subjects;
+using System.Threading;
 
 namespace stickypipes.api.Services
 {
     public class CacheService
     {
-        public void Create(Guid sessionId, string userName)
-        {
-            SessionCache.SessionIdToOtherCacheLookups.Add(sessionId, userName);
-        }
+        private readonly SemaphoreSlim _updateValueLock = new SemaphoreSlim(1, 1);
 
-        public void Delete()
-        {
+        private readonly ConcurrentDictionary<Guid, Session> _sessions = new ConcurrentDictionary<Guid, Session>();
+        private readonly ConcurrentDictionary<Guid, Subject<Session>> _subjects = new ConcurrentDictionary<Guid, Subject<Session>>();
 
+        private readonly TimeSpan _updateInterval = TimeSpan.FromSeconds(1);
+
+        private Timer _timer;
+        private volatile bool _updatingValues;
+
+        public void Create(Guid sessionId, string user)
+        {
+            var session = new Session() { Id = Guid.NewGuid(), User = user };
+            _sessions.TryAdd(sessionId, session);
+            _subjects.TryAdd(sessionId, new Subject<Session>());
+
+            if (_timer == null)
+            {
+                _timer = new Timer(UpdateValues, null, _updateInterval, _updateInterval);
+            }
         }
 
         public bool TryGet(Guid id, out string username)
         {
-            if (SessionCache.SessionIdToOtherCacheLookups.TryGetValue(id, out username))
+            username = "";
+            if (!_sessions.TryGetValue(id, out Session session))
             {
-                return true;
+                return false;
             }
-
-            return false;
+            username = session.User;
+            return true;
         }
 
-        public List<string> GetAll()
+        public IObservable<Session> StreamValues(Guid id)
         {
-            return SessionCache.SessionIdToOtherCacheLookups.Values.ToList();
+            return _subjects[id];
+        }
+
+        private async void UpdateValues(object state)
+        {
+            await _updateValueLock.WaitAsync();
+            try
+            {
+                if (!_updatingValues)
+                {
+                    _updatingValues = true;
+
+                    foreach (var session in _sessions)
+                    {
+                        session.Value.Value++;
+                        if (_subjects.TryGetValue(session.Key, out Subject<Session> subject))
+                        {
+                            subject.OnNext(session.Value);
+                        }
+                    }
+
+                    _updatingValues = false;
+                }
+            }
+            finally
+            {
+                _updateValueLock.Release();
+            }
         }
     }
 }
--- stickypipes.api/Startup.cs
+++ stickypipes.api/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using stickypipes.api.Hubs;
 using stickypipes.api.Services;
 
@@ -20,23 +19,16 @@ namespace stickypipes.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.AddSignalR();
+
             services.AddSingleton<CacheService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseFileServer();
-
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapHub<StickyHub>("/api/hubs/sticky");
